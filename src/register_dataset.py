f49467173ebe33026b40dfac2ceaaf95afacec26
tkg11@case.edu <>
2021-05-06 10:16:50 -0400

    generalize register_dataset.py

--- src/register_dataset.py
+++ src/register_dataset.py
@@ -1,4 +1,17 @@
-from detectron2.data.datasets import register_coco_instances
+import sys
 
-register_coco_instances('baxter_coco_train1', {}, "../baxter_dataset/annotations/baxter_coco_train.json", "dataset/train")
-register_coco_instances('baxter_coco_val1', {}, "../baxter_dataset/annotations/baxter_coco_val.json", "dataset/val")
+
+def register(train_name, val_name):
+    from detectron2.data.datasets import register_coco_instances
+
+    register_coco_instances(train_name, {}, "../baxter_dataset/annotations/baxter_coco_train.json",
+                            "dataset/train")
+    register_coco_instances(val_name, {}, "../baxter_dataset/annotations/baxter_coco_val.json", "dataset/val")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Must pass in a training name and validation name to register baxter_coco dataset under')
+        print('Usage:\npython register_dataset train_name val_name')
+    else:
+        register(sys.argv[1], sys.argv[2])
